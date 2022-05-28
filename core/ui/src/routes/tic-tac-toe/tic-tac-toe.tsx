3f0b6962e9ef0b424c15782609d275835b4be3ae
Peter Carter <peter.carter143@gmail.com>
2022-05-28 18:47:05 -0500

    aiMove

--- core/ui/src/routes/tic-tac-toe/tic-tac-toe.tsx
+++ core/ui/src/routes/tic-tac-toe/tic-tac-toe.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 import './tic-tac-toe.scss';
@@ -7,9 +7,9 @@ import { Board } from '../../common/board/board';
 import { GameLayout } from '../../common/game-layout/game-layout';
 
 type State = {
-    board: (String | null)[][],
-    turn: Number,
-}
+    board: (String | null)[][];
+    turn: Number;
+};
 
 export const TicTacToeRoute = () => {
     const [state, setState] = useState<State>({
@@ -17,34 +17,41 @@ export const TicTacToeRoute = () => {
         turn: 1,
     });
     const [response, setResponse] = useState<State | null>(null);
-    useEffect(() => {
-        if (response) {
-            setState(response);
-        }
-    }, [response]);
 
     const playerMove = (cell: [0 | 1 | 2, 0 | 1 | 2]): void => {
         axios
-            .post('http://localhost:8000/api/tictactoe/move', {...state, cell})
+            .post('http://localhost:8000/api/tictactoe/move', {
+                ...state,
+                cell,
+            })
             .then((res) => {
-                console.log("response", res);
-                setResponse({board: res.data, turn: -state.turn});
+                console.log('response', res);
+                setResponse({ board: res.data, turn: -state.turn });
             })
             .catch((err) => {
                 console.log('Error from playerMove', err);
             });
-    }
-    const aiMove = (): void => {
+    };
+    const aiMove = useCallback((): void => {
         axios
             .post('http://localhost:8000/api/tictactoe/ai', state)
             .then((res) => {
-                console.log("response", res);
-                setResponse(res.data);
+                console.log('response', res);
+                setResponse({ board: res.data, turn: -state.turn });
             })
             .catch((err) => {
                 console.log('Error from aiMove', err);
             });
-    };
+    }, [state]);
+
+    useEffect(() => {
+        if (response) {
+            setState(response);
+            if (state.turn === -1) {
+                aiMove();
+            }
+        }
+    }, [response, state, aiMove]);
 
     return (
         <div className="tic-tac-toe-route">
