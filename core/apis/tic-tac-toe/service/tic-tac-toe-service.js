e506acee435bbd90737cdff914b8267aef2f5377
Peter Carter <peter.carter143@gmail.com>
2022-05-28 14:14:26 -0500

    add typescript to server

--- core/apis/tic-tac-toe/service/tic-tac-toe-service.js
+++
@@ -1,7 +0,0 @@
-
-
-const aiMove = ({ cells, player }) => {
-    return cells;
-};
-
-module.exports = { aiMove };
---
+++ core/apis/tic-tac-toe/service/tic-tac-toe-service.tsx
@@ -0,0 +1,5 @@
+const aiMove = ({ cells, player }) => {
+    return cells;
+};
+
+module.exports = { aiMove };
